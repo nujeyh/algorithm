1247152a73cc9cdb2471b097d5f055d198c0fd3f
hyejun <102746846+nujeyh@users.noreply.github.com>
2022-08-03 03:51:58 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split('\n');
+
+console.log(+input[0] * +input[1][2]);
+console.log(+input[0] * +input[1][1]);
+console.log(+input[0] * +input[1][0]);
+console.log(+input[0] * +input[1]);
