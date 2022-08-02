6c20bd6ad210b0df6d12820de79bea2f5407e143
hyejun <102746846+nujeyh@users.noreply.github.com>
2022-08-03 03:35:28 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('dev/stdin').toString().split(" ");
+
+const A = +input[0]
+const B = +input[1]
+const C = +input[2]
+
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
