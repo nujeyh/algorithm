9d0e1128d5794010039f204ba2641cf422d2beb5
nujeyh <dza456@naver.com>
2022-08-08 14:16:07 +0900

    Del

--- 프로그래머스/1단계/x만큼 간격이 있는 n개의 숫자/solution.js
+++
@@ -1,8 +0,0 @@
-function solution(x, n) {
-  let answer = [];
-
-  for (let i = 0; i < n; i++) {
-    answer[i] = x * (i + 1);
-  }
-  return answer;
-}
--- 프로그래머스/1단계/직사각형 별찍기/solution.js
+++
@@ -1,7 +0,0 @@
-process.stdin.setEncoding("utf8");
-process.stdin.on("data", (data) => {
-  const n = data.split(" ");
-  for (let i = 0; i < +n[1]; i++) {
-    console.log("*".repeat(+n[0]));
-  }
-});
