83b02becf972e351d518c76a389e9dacc3bd9d48
Hyejun <dza456@naver.com>
2022-08-12 21:13:56 +0900

    [level 1] Title: 약수의 합, Time: 0.09 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let answer = n ;
+    
+    for (let i = 1; i <= n/2; i++) {
+        if (n % i === 0) {
+            answer += i;
+        }
+    }
+    return answer;
+}
