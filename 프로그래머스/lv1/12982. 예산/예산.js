7ac3a0db912212de8b432f29d7a68ba97dc685b4
Hyejun <dza456@naver.com>
2022-08-23 17:23:46 +0900

    [level 1] Title: 예산, Time: 0.10 ms, Memory: 30 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -0,0 +1,9 @@
+function solution(d, budget) {
+    let answer = 0;
+    d.sort((a, b) => a - b);
+    for (let i = 0; i < d.length; i++) {
+        budget -= d[i];
+        if (budget >= 0) answer += 1;
+    }
+    return answer;
+}
