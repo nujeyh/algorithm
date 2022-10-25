99c3d8eaf42f697b8d2f2453d08f10014859ba0e
Hyejun <dza456@naver.com>
2022-10-25 14:54:10 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.80 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,12 @@
+function solution(A,B){
+    let answer = 0;
+    
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    
+    A.forEach((el, i) => {
+        answer += el * B[i];
+    })
+
+    return answer;
+}
