0a249adce452fb1bf4999e23c70384416784170a
Hyejun <dza456@naver.com>
2023-03-03 16:26:24 +0900

    [level 2] Title: 구명보트, Time: 11.65 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.js
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.js
@@ -1,10 +1,10 @@
 function solution(people, limit) {
+    people.sort((a, b) => a - b);
+
     let boatCount = 0;
     let left = 0;
     let right = people.length - 1;
-    
-    people.sort((a, b) => a - b);
-    
+        
     while (left <= right) {
         if (people[left] + people[right] <= limit) left++;
         right--;
